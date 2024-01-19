113363eea2df3a7f6a3a8aba07f905724f79a1f7
Venus <venusbastola27@gmail.com>
2024-01-19 22:19:17 +0545

    cors added

--- base-server/app.js
+++ base-server/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const dbConnect = require("./server");
+const cors = require("cors");
 const userRouter = require("./routes/userRoutes");
 
 dbConnect();
 const app = express();
+app.use(cors());
 app.use(express.json()); // middle ware for parsing body object..
 app.use("/api/v1/users", userRouter);
 //app.use("/api/v1/prediction", predictionRouter);
