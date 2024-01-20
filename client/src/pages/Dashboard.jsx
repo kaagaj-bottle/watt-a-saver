2f7381372b86bf6d8bb85eac9e5995985c8a3f2d
Zishan Siddique <zishansiddique80@gmail.com>
2024-01-20 09:43:10 +0545

    dashboard grid working

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,13 +1,37 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import { styleObject } from '../utils/globalConsts'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+import { styleObject } from '../utils/globalConsts';
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 export default function Dashboard() {
   return (
-    <>
-      <Box sx={styleObject}>
-        This is the dashboard
+    <Box sx={{ styleObject }}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
       </Box>
-    </>
-  )
+    </Box>
+  );
 }
-
